9947e994e83823f0ef657436b7edaf9df2d64fa2
keji <tomitoyo.93@gmail.com>
2023-08-10 22:24:00 +0100

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  console.log('Home', 'Home');
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
